8e36272452b4a9c3603d8dc9260212d1fba1df32
Chstphr <somto565@gmail.com>
2022-12-30 11:34:40 +0100

    imported image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlines, MoneyCollectOutlined, BulbOutlined,FundOutlined, MenuOutlined } from'@ant-design/icons';
-
+import icon from '../images/cryptocurrency.png';
 const Navbar = () => {
     return (
         <div className='nav-container'>
             <div className='logo-container'>
-                <Avatar />
+                <Avatar src={icon} size="large" />
                 <Typography.Title level={2} className="logo">
                     <Link to="/">Cryptoverse</Link>
                 </Typography.Title>
