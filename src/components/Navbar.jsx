6c9b4dde4ecada8a03031d18f958872ff611fe48
Chstphr <somto565@gmail.com>
2022-12-31 07:17:30 +0100

    successfully added image and header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import { Button, Menu, Typography, Avatar } from 'antd';
-import { Link } from 'react-router-dom';
-import { HomeOutlines, MoneyCollectOutlined, BulbOutlined,FundOutlined, MenuOutlined } from'@ant-design/icons';
-import icon from '../images/cryptocurrency.png';
-const Navbar = () => {
-    return (
-        <div className='nav-container'>
-            <div className='logo-container'>
-                <Avatar src={icon} size="large" />
-                <Typography.Title level={2} className="logo">
-                    <Link to="/">Cryptoverse</Link>
-                </Typography.Title>
-                {/* <Button className='menu-control-control'>
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd'
+import { Link } from 'react-router-dom'
+import { HomeOutlined, MoneyCollecOutlined, BulbOutlined, FundOutlines, MenuOutlined } from '@ant-design/icons';
+// import img1 from './cryptocurrency.png';
 
-                </Button> */}
-            </div>
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+        <div className='logo-container'>
+            <img src="https://i.ibb.co/Z11pcGG/cryptocurrency.png" size='large' alt='img'/>
+            <Typography.Title level={2} className="logo">
+                <Link to="/">Cryptoverse</Link>
+                {/* <h1>ccccccc</h1> */}
+            </Typography.Title>
+            {/* <Button className='menu-control-container'>
+
+            </Button> */}
         </div>
-    )
+    </div>
+    
+  )
 }
 
 export default Navbar
