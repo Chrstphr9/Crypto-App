8453310656954738580993008bf142dc26b5a5c5
Chstphr <somto565@gmail.com>
2023-01-06 16:49:01 +0100

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd'
 import { Link } from 'react-router-dom'
-import { HomeOutlined, MoneyCollecOutlined, BulbOutlined, FundOutlines, MenuOutlined } from '@ant-design/icons';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlines, MenuOutlined, FundOutlined } from '@ant-design/icons';
 // import img1 from './cryptocurrency.png';
 
 const Navbar = () => {
@@ -13,10 +13,21 @@ const Navbar = () => {
                 <Link to="/">Cryptoverse</Link>
                 {/* <h1>ccccccc</h1> */}
             </Typography.Title>
-            {/* <Button className='menu-control-container'>
-
-            </Button> */}
         </div>
+        <Menu theme="dark">
+            <Menu.Item icon={<HomeOutlined />}>
+                <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item icon={<FundOutlined />}>
+                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            </Menu.Item>
+            <Menu.Item icon={<MoneyCollectOutlined />}>
+                <Link to="/exchanges">Exchanges</Link>
+            </Menu.Item>
+            <Menu.Item icon={<BulbOutlined />}>
+                <Link to="/news">News</Link>
+            </Menu.Item>
+        </Menu>
     </div>
     
   )
